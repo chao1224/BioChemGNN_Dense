e666734b44fecef1b83a2bcb53c09161fecad168
chao1224 <shengchao.hacker@gmail.com>
2020-07-10 06:40:47 -0700

    Update bash script

--- 0626/download.sh
+++ 0626/download.sh
@@ -2,11 +2,16 @@ mkdir -p datasets
 
 cd datasets
 
-wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/muv.csv.gz
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/tox21.csv.gz
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/clintox.csv.gz
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/toxcast_data.csv.gz
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/BBBP.csv
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/sider.csv.gz
+
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/muv.csv.gz
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/HIV.csv
-mv HIV.csv hiv.csv
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/pcba.csv.gz
+wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/bace.csv
 
 wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/delaney-processed.csv
 wget -O malaria-processed.csv https://raw.githubusercontent.com/HIPS/neural-fingerprint/master/data/2015-06-03-malaria/malaria-processed.csv
